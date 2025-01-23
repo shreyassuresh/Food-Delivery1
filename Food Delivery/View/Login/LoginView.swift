6b947d849995ba8c88ae3c7579f90641ba9e2f4c
shreyassuresh <shreyas30j@gmail.com>
2025-01-23 19:09:40 +0530

    Swift UI Update: Login Page

--- Food Delivery/View/Login/LoginView.swift
+++ Food Delivery/View/Login/LoginView.swift
@@ -1,13 +1,12 @@
-//
-//  LoginView.swift
-//  Food Delivery
-//
-//  Created by student on 20/01/25.
-//
+
 
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email: String = ""
+    
+       
+    
     var body: some View {
         ZStack{
             Image("bottom_bg")
@@ -22,9 +21,42 @@ struct LoginView: View {
                     .frame(width: 55, height: 55)
                     .padding(.top,60)
                 Spacer()
-               
+                Text("Sign in")
+                    .font(.system(size: 35))
+                    .padding(.leading,-150)
+                    .bold()
+                    .padding(.top,50)
                     
+                Text("Enter your email and password")
+                    .font(.system(size: 20))
+                    
+                    .padding(.leading,-30)
+                    .foregroundColor(.gray)
+                TextField("Enter your email ",text: $email)
+                    
+                    .padding(.leading,48)
+                    .padding(.top,30)
+                Rectangle()
+                    .frame(height: 1)
+                    .foregroundColor(.gray.opacity(0.3))
+                    .padding(.horizontal, 30)
+                    .padding(.bottom)
+                TextField("Enter your password", text: $email)
+                    .padding(.leading,48)
+                Rectangle()
+                    .frame(height: 1)
+                    .foregroundColor(.gray.opacity(0.3))
+                    .padding(.horizontal, 30)
+                    .padding(.bottom)
+                
+                
+                    
+                
+                
+ 
+             
             }
+            .padding(.bottom, 550)
         }
     }
 }
