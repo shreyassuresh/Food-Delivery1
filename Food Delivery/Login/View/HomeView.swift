361b7c1eb5a6e5de9608f594e4989d37d881daeb
shreyassuresh <shreyas30j@gmail.com>
2025-02-07 18:55:20 +0530

    Changes

--- Food Delivery/Login/View/HomeView.swift
+++ Food Delivery/Login/View/HomeView.swift
@@ -120,6 +120,69 @@ struct HomeView: View {
                             .foregroundColor(.green)
                             .padding(.leading,119)
                     }
+                    ScrollView(.horizontal, showsIndicators: false) {
+                            
+                        
+                            HStack{
+                                Image("banana")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                    .overlay(
+                                                                    Rectangle()
+                                                                        .fill(Color.black.opacity(0.1)) // Semi-transparent rectangle
+                                                                        .frame(width: 100, height: 100)
+                                                                        .cornerRadius(10)
+                                                                )
+                                Image("add_green")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 20, height: 20)
+                                    .overlay(
+                                                                    Rectangle()
+                                                                        .fill(Color.black.opacity(0.1)) // Semi-transparent rectangle
+                                                                        .frame(width: 30, height: 30)
+                                                                        .cornerRadius(10)
+                                                                )
+                                VStack(alignment: .leading){
+                                    Text("Banana")
+                                        .font(.system(size: 17, weight: .medium))
+                                    Text("Rs. 99")
+                                        .font(.system(size: 13))
+                                }
+                           
+                            Image("apple_red")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 100)
+                                .overlay(
+                                                                Rectangle()
+                                                                    .fill(Color.black.opacity(0.1)) // Semi-transparent rectangle
+                                                                    .frame(width: 100, height: 100)
+                                                                    .cornerRadius(10)
+                                                            )
+                                Image("add_green")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 20, height: 20)
+                                    .overlay(
+                                                                    Rectangle()
+                                                                        .fill(Color.black.opacity(0.1)) // Semi-transparent rectangle
+                                                                        .frame(width: 30, height: 30)
+                                                                        .cornerRadius(10)
+                                                                )
+                                
+                                VStack(alignment: .leading){
+                                    Text("Apple Red")
+                                        .font(.system(size: 17, weight: .medium))
+                                    Text("Rs. 169")
+                                        .font(.system(size: 13))
+                                }
+                                
+                                   
+                                    }
+                                //testing
+                            }
 
                     }
 //                    .padding(.top, 20)
