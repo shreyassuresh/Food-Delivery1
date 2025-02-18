f916aca2a229147a8550a33473873f2839cf9e32
shreyassuresh <shreyas30j@gmail.com>
2025-02-18 17:00:56 +0530

    Swift UI Update: Home View2

--- Food Delivery/Login/View/HomeView2.swift
+++ Food Delivery/Login/View/HomeView2.swift
@@ -1,101 +1,184 @@
-//
-//  HomeView2.swift
-//  Food Delivery
-//
-//  Created by student on 17/02/25.
-//
-
 import SwiftUI
-struct Product: Identifiable{
+
+struct Product: Identifiable {
     let id = UUID()
-    let name : String
+    let name: String
     let imageName: String
     let price: String
 }
 
 struct HomeView2: View {
+    @State private var searchQuery: String = ""
+    
     let products = [
         Product(name: "Organic Bananas", imageName: "banana", price: "100 Rs"),
         Product(name: "Red Apple", imageName: "apple_red", price: "120 Rs"),
         Product(name: "Bell Pepper", imageName: "bell_pepper_red", price: "100 Rs"),
         Product(name: "Ginger", imageName: "ginger", price: "120 Rs"),
     ]
+    
     var body: some View {
-       NavigationView {
-           ScrollView{
-               VStack{
-                   Image("color_logo")
-                       .resizable()
-                       .scaledToFit()
-                       .frame(width: 50)
-                   HStack{
-                       Spacer()
-                       Image("location")
-                           .resizable()
-                           .scaledToFit()
-                           .frame(width: 20,height: 30)
-                       Text("Vadodara, India")
-                           .font(.headline)
-                           .fontWeight(.bold)
-                       Spacer()
-                   }
-                   .padding()
-                   
-                   Image("banner_top")
-                       .resizable()
-                       .scaledToFit()
-                       .padding(.horizontal)
-                   Text("Exclusive Offers")
-                       .font(.headline)
-                       .padding(.leading)
-                   
-                   ScrollView(.horizontal, showsIndicators: false){
-                       HStack{
-                           ForEach(products.suffix(2)){product in
-                               ProductView(product: product)
-                           }
+        NavigationView {
+            ScrollView {
+                VStack {
+                    // Logo and Location
+                    Image("color_logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50)
+                    
+                    HStack {
+                        Image("location")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20, height: 30)
+                        Text("Vadodara, India")
+                            .font(.headline)
+                            .fontWeight(.bold)
+                    }
+                    .padding()
+                    
+                    // Search Bar
+                    HStack {
+                        Image("search")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 18, height: 30)
+                            .padding(.leading, 30)
                         
-                       }
-                       .padding(.horizontal)
-                   }
-                   Text("Groceries")
-                       .font(.headline)
-                       .padding(.leading)
-                       
-               }
-           }
-           .navigationBarTitle("Nectar", displayMode: .inline)
+                        TextField("Search here...", text: $searchQuery)
+                            .padding(.leading, 7)
+                            .background(Color.gray.opacity(0.1))
+                            .cornerRadius(7)
+                            .frame(height: 45)
+                            .padding(.leading, 3)
+                            .padding(.trailing, 30)
+                    }
+                    
+                    // Banner Image
+                    Image("banner_top")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 350)
+                    
+                    // Exclusive Offers Section
+                    HStack {
+                        Text("Exclusive Offer")
+                            .font(.system(size: 23, weight: .semibold))
+                        
+                        Spacer()
+                        
+                        Text("See all")
+                            .foregroundColor(.green)
+                    }
+                    .padding(.horizontal)
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
+                            ForEach(products.prefix(2)) { product in
+                                ProductView(product: product)
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+                    
+                    // Best Selling Section
+                    HStack {
+                        Text("Best Selling")
+                            .font(.system(size: 23, weight: .semibold))
+                        
+                        Spacer()
+                        
+                        Text("See all")
+                            .foregroundColor(.green)
+                    }
+                    .padding(.horizontal)
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
+                            ForEach(products.suffix(2)) { product in
+                                ProductView(product: product)
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+                    
+                    // Groceries Section
+                    HStack {
+                        Text("Groceries")
+                            .font(.system(size: 23, weight: .semibold))
+                        
+                        Spacer()
+                        
+                        Text("See all")
+                            .foregroundColor(.green)
+                    }
+                    .padding(.horizontal)
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
+                            Image("pulses")
+                                .resizable()
+                                .frame(width: 60, height: 60)
+                            
+                            VStack(alignment: .leading) {
+                                Text("Pulses")
+                                    .font(.system(size: 17, weight: .medium))
+                            }
+                            .padding()
+                            
+                            Image("rice")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 60, height: 60)
+                            
+                            VStack(alignment: .leading) {
+                                Text("Rice")
+                                    .font(.system(size: 17, weight: .medium))
+                            }
+                            .padding()
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle("Nectar", displayMode: .inline)
         }
     }
 }
-struct ProductView:View{
+
+struct ProductView: View {
     let product: Product
+    
     var body: some View {
-        VStack(alignment: .leading){
+        VStack(alignment: .leading) {
             Image(product.imageName)
                 .resizable()
                 .frame(width: 100, height: 100)
                 .scaledToFit()
-            Text(product.name)
-                .font(.caption)
-                .padding(.top, 5)
-            Text("\(product.price)")
-                .font(.headline)
-                .foregroundColor(.green)
-            Button(action: {
                 
+            
+            VStack(alignment: .leading) {
+                Text(product.name)
+                    .font(.system(size: 17, weight: .medium))
+                    .lineLimit(1)
+                Text(product.price)
+                    .font(.system(size: 13))
+            }
+            
+            Button(action: {
+                // Add product to cart
             }) {
                 Image(systemName: "plus.circle.fill")
                     .foregroundColor(.green)
             }
-            .padding(.top,5)
+            .padding(.top, 5)
         }
         .padding()
         .background(Color(.systemGray6))
         .cornerRadius(10)
-        
     }
 }
+
 #Preview {
     HomeView2()
 }
