5e0f62b54f5a6ef3879ba56f33bc2f1591625400
shreyassuresh <shreyas30j@gmail.com>
2025-02-22 14:34:31 +0530

    Updated HomeView2

--- Food Delivery/Login/View/HomeView2.swift
+++ Food Delivery/Login/View/HomeView2.swift
@@ -5,7 +5,11 @@ struct Product: Identifiable {
     let name: String
     let imageName: String
     let price: String
+    let description: String
+    let nutritionInfo: String
+    let rating: Double
 }
+
 
 enum Tab {
     case shop
