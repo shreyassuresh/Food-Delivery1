12f30d4935bc0ef8b5d4d29233761dfa362799c2
shreyassuresh <shreyas30j@gmail.com>
2025-02-25 12:34:04 +0530

    Swift UI Update: ExploreView tab Changes

--- Food Delivery/Login/View/ExploreView.swift
+++ Food Delivery/Login/View/ExploreView.swift
@@ -10,11 +10,11 @@ struct Category: Identifiable {
 struct ExploreView: View {
     let categories = [
         Category(imageName: "fruits_vegetables", title: "Fresh Fruits & Vegetable", backgroundColor: .green.opacity(0.2)),
-              Category(imageName: "cooking_oil", title: "Cooking Oil & Ghee", backgroundColor: .yellow.opacity(0.2)),
-              Category(imageName: "meat_fish", title: "Meat & Fish", backgroundColor: .red.opacity(0.2)),
-              Category(imageName: "bakery_snacks", title: "Bakery & Snacks", backgroundColor: .purple.opacity(0.2)),
-              Category(imageName: "dairy_eggs", title: "Dairy & Eggs", backgroundColor: .blue.opacity(0.2)),
-              Category(imageName: "beverages", title: "Beverages", backgroundColor: .orange.opacity(0.2))
+        Category(imageName: "cooking_oil", title: "Cooking Oil & Ghee", backgroundColor: .yellow.opacity(0.2)),
+        Category(imageName: "meat_fish", title: "Meat & Fish", backgroundColor: .red.opacity(0.2)),
+        Category(imageName: "bakery_snacks", title: "Bakery & Snacks", backgroundColor: .purple.opacity(0.2)),
+        Category(imageName: "dairy_eggs", title: "Dairy & Eggs", backgroundColor: .blue.opacity(0.2)),
+        Category(imageName: "beverages", title: "Beverages", backgroundColor: .orange.opacity(0.2))
     ]
     
     var body: some View {
@@ -23,30 +23,44 @@ struct ExploreView: View {
             categoriesGrid
         }
     }
-
+    
     private var headerView: some View {
         VStack {
             Text("Find Products")
                 .font(.largeTitle)
                 .padding()
-
+            
             TextField("Search Store", text: .constant(""))
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
         }
     }
-
+    
     private var categoriesGrid: some View {
         ScrollView {
-            LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 20) {
-                ForEach(categories) { category in
-                    CategoryBoxView(imageName: category.imageName, title: category.title, backgroundColor: category.backgroundColor)
+            VStack{
+                HStack(spacing: 20) {
+                    CategoryBoxView(imageName: "frash_fruits", title: "Fresh Fruits & Vegetable", backgroundColor: .green.opacity(0.1))
+                    CategoryBoxView(imageName: "cooking_oil", title: "Cooking Oil & Ghee", backgroundColor: .pink.opacity(0.1))
                 }
+                
+                HStack(spacing: 20) {
+                    CategoryBoxView(imageName: "meat_fish", title: "Meat & Fish", backgroundColor: .brown.opacity(0.1))
+                    CategoryBoxView(imageName: "cooking_oil", title: "Fresh Fruits & Vegetable", backgroundColor: .red.opacity(0.1))
+                }
+                HStack(spacing: 20) {
+                    CategoryBoxView(imageName: "meat_fish", title: "Meat & Fish", backgroundColor: .brown.opacity(0.1))
+                    CategoryBoxView(imageName: "cooking_oil", title: "Fresh Fruits & Vegetable", backgroundColor: .red.opacity(0.1))
+                }
+                
             }
-            .padding()
         }
+        
+        .padding()
     }
 }
+    
+
 
 struct CategoryView: View {
     let category: Category
